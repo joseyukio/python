25c3916280a39175bc08720073a72e4a4e6cbb15
Jose Yukio Akazawa <joseyukio@gmail.com>
2017-10-07 19:47:09 -0300

    First draft. Just the quicksort algorithm.

---
+++ numpy.py
@@ -0,0 +1,14 @@
+# CS231
+# http://cs231n.github.io/python-numpy-tutorial/
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+
